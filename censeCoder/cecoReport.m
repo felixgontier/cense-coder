24c5d44f98fe5bf8a8b4d8c625c36e45e2cc2619
felixgontier <gontier.felix@gmail.com>
2017-04-29 14:40:49 +0200

    Initial commit

--- censeCoder/cecoReport.m
+++ censeCoder/cecoReport.m
@@ -6,6 +6,11 @@ function config = cecoReport(config)
 % Copyright: felix                                          
 % Date: 20-Apr-2017                                         
                                                             
-if nargin==0, censeCoder('report', 'rhv'); return; end      
+if nargin==0, censeCoder('report', 'r'); return; end%censeCoder('report', 'rhv'); return; end      
                                                             
-config = expExpose(config, 't');                            
+config = expExpose(config, 'p', 'mask', {0, 0, 6, 1, 2, 10, 2, 4}, 'obs', 'cv_acc', 'step', 3, 'color', 'k'); % KNN accuracy as f(mel,fps)
+config = expExpose(config, 'p', 'mask', {0, 0, 6, 1, 2, 10, 1, 4}, 'obs', 'csii', 'step', 3, 'expand', 2, 'color', 'k'); % CSII indicator as f(mel,fps)
+config = expExpose(config, 'p', 'mask', {0, 0, 6, 1, 2, 10, 2, 4}, 'obs', 'bitrate', 'step', 2, 'expand', 2, 'color', 'k'); % bitrate as f(mel,fps)
+
+
+
