23873bde40e4395fe37cbe1297fca49851343848
nicolas-f <[omitted]>
2017-10-18 14:29:46 +0200

    Add proper cmake unit test

--- embedded/stm/audio_analyzer/acoustic_indicators.h
+++ embedded/stm/audio_analyzer/acoustic_indicators.h
@@ -20,7 +20,7 @@ int ai_get_maximal_sample_size(const AcousticIndicatorsData* data);
 
 /**
  * Add sample to the processing chain
- * @param[in-out] data instance of this struct, create an empty struct on first use
+ * @param[in,out] data instance of this struct, create an empty struct on first use
  * @param[in] sample_data sample content to add
  * @param[in] sample_len sample length of sample_data. Must be < than ai_get_maximal_sample_size
  * @param[out] laeq 1s laeq value if the return is true
--- embedded/stm/tests/test_acoustic_indicators.c
+++ embedded/stm/tests/test_acoustic_indicators.c
@@ -8,12 +8,13 @@
 int main(int argc, char **argv) {
 	const char *filename = "speak_32000Hz_16bitsPCM_10s.raw";
 	FILE *ptr;
+	AcousticIndicatorsData acousticIndicatorsData();
 	unsigned char buffer[128];
 
 	// open file
 	ptr = fopen(filename, "rb");
 	if (ptr == NULL) {
-		printf("Error opening file\n");
+		printf("Error opening audio file\n");
 		exit(1);
 	}
 
@@ -22,5 +23,5 @@ int main(int argc, char **argv) {
 	while(!feof(ptr)) {
 		read = fread(buffer, sizeof(buffer), 1, ptr);
 
-	}	
+	}
 }
