de881483ab9ed5c9784e50b21912812a7df4f47f
nicolas-f <[omitted]>
2017-10-19 15:01:59 +0200

    Too much iteration, inspecting..

--- embedded/stm/tests/test_acoustic_indicators.c
+++ embedded/stm/tests/test_acoustic_indicators.c
@@ -10,8 +10,12 @@ int tests_run = 0;
  * Read raw audio signal file
  */
 static char * test_leq_32khz() {
-  double RMS_REFERENCE_90DB = 2500;
-  double DB_FS_REFERENCE = - (20 * log10(RMS_REFERENCE_90DB)) + 90;
+  // Compute the reference level.
+  //double RMS_REFERENCE_90DB = 2500;
+  //double DB_FS_REFERENCE = - (20 * log10(RMS_REFERENCE_90DB)) + 90;
+  //double REF_SOUND_PRESSURE = 1 / pow(10, DB_FS_REFERENCE / 20);
+
+  double REF_SOUND_PRESSURE = 32767.;
 
 	const char *filename = "speak_32000Hz_16bitsPCM_10s.raw";
 	FILE *ptr;
@@ -27,21 +31,23 @@ static char * test_leq_32khz() {
 		exit(1);
 	}
 
+  int total_read = 0;
 	int read = 0;
   float leqs[10];
   int leqId = 0;
 
 	while(!feof(ptr)) {
 		read = fread(shortBuffer, sizeof(int16_t), sizeof(shortBuffer) / sizeof(int16_t), ptr);
+    total_read+=read;
 		// File fragment is in read array
-
 		// Process short sample
 		int sampleCursor = 0;
 		do {
 			int maxLen = ai_GetMaximalSampleSize(&acousticIndicatorsData);
-			int sampleLen = read < maxLen ? read : maxLen;
+			int sampleLen = (read - sampleCursor) < maxLen ? (read - sampleCursor) : maxLen;
 			float leq;
-			if(ai_AddSample(&acousticIndicatorsData, sampleLen, shortBuffer + sampleCursor, &leq,DB_FS_REFERENCE, false)) {
+			if(ai_AddSample(&acousticIndicatorsData, sampleLen, shortBuffer + sampleCursor, &leq,REF_SOUND_PRESSURE, false)) {
+        mu_assert("Too much iteration", leqId < 10);
         leqs[leqId++] = leq;
 			}
 			sampleCursor+=sampleLen;
