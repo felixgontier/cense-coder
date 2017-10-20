65057de6ad588340a25cb8fe821b1a6fde581520
nicolas-f <[omitted]>
2017-10-20 13:16:39 +0200

    Add A filtering

--- embedded/stm/audio_analyzer/acoustic_indicators.c
+++ embedded/stm/audio_analyzer/acoustic_indicators.c
@@ -4,6 +4,22 @@
 #include <math.h>
 #include <string.h>
 
+
+
+// int order = max(denominator.length, numerator.length);
+#define ORDER (7)
+
+/**
+ * Numerator coefficients of the A-weighting filter determined by means of a bilinear transform that converts
+ * second-order section analog weights to second-order section digital weights.
+ */
+const float_t numerator_32khz[ORDER] = {0.34345834, -0.68691668, -0.34345834, 1.37383335, -0.34345834, -0.68691668, 0.34345834};
+/**
+ * Denominator coefficients of the A-weighting filter determined by means of a bilinear transform that converts
+ * second-order section analog weights to second-order section digital weights.
+ */
+const float_t denominator_32khz[ORDER] = {1. , -3.65644604, 4.83146845, -2.5575975, 0.25336804, 0.12244303, 0.00676407};
+
 int ai_GetMaximalSampleSize(const AcousticIndicatorsData* data) {
 	return AI_WINDOW_SIZE - data->window_cursor;
 }
@@ -18,7 +34,25 @@ bool ai_AddSample(AcousticIndicatorsData* data, int sample_len, const int16_t* s
 	if(data->window_cursor >= AI_WINDOW_SIZE) {
 		data->window_cursor = 0;
 		// Compute A weighting using band-pass filter
-		
+		if(a_filter) {
+				float_t weightedSignal[AI_WINDOW_SIZE];
+				// Filter delays
+				float_t z[ORDER-1][AI_WINDOW_SIZE];
+				for (int idT = 0; idT < AI_WINDOW_SIZE; idT++){
+            // Avoid iteration idT=0 exception (z[0][idT-1]=0)
+            weightedSignal[idT] = (numerator_32khz[0]*data->window_data[idT] + (idT == 0 ? 0 : z[0][idT-1]));
+            // Avoid iteration idT=0 exception (z[1][idT-1]=0)
+            z[0][idT] = (numerator_32khz[1]*data->window_data[idT] + (idT == 0 ? 0 : z[1][idT-1]) - denominator_32khz[1]*data->window_data[idT]);
+            for (int k = 0; k<ORDER-2; k++){
+                // Avoid iteration idT=0 exception (z[k+1][idT-1]=0)
+                z[k][idT] = (numerator_32khz[k+1]*data->window_data[idT] + (idT ==0 ? 0 : z[k+1][idT-1]) - denominator_32khz[k+1]*weightedSignal[idT]);
+            }
+            z[ORDER-2][idT] = (numerator_32khz[ORDER-1]*data->window_data[idT] - denominator_32khz[ORDER-1] * weightedSignal[idT]);
+        }
+				for (int idT = 0; idT < AI_WINDOW_SIZE; idT++){
+					data->window_data[idT] = weightedSignal[idT];
+				}
+		}
 		// Compute RMS
 		long sampleSum = 0;
 		for(int i=0; i < AI_WINDOW_SIZE; i++) {
